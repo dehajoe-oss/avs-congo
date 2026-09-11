2e7370ba397df3a41998d6ad08b103ed10c84a03
johaoooo <josephdehazounde@gmail.com>
2026-09-11 16:25:17 +0100

    perf(fonts): optimise le chargement des polices Poppins et reduit les preloads

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,8 @@ const VisitorTracker = dynamic(() => import('@/components/ui/VisitorTracker'), {
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  style: ['normal', 'italic'],
+  weight: ['400', '500', '600', '700', '800'],
+  style: ['normal'],
   display: 'swap',
   variable: '--font-poppins',
 })
